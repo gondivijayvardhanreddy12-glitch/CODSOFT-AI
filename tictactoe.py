e1acb73bd658ba2d83dded34ffd8d8afe58b7bd0
gondivijayvardhanreddy12-glitch <gondivijayvardhanreddy12@gmail.com>
2026-05-11 21:22:39 +0530

    tictactoe.py
           TASK 2
    TIC-TAC-TOE AI

--- tictactoe.py
+++ tictactoe.py
@@ -3,7 +3,6 @@ import math
 HUMAN = "X"
 AI = "O"
 
-# Create board
 def create_board():
     return [" " for _ in range(9)]
 
@@ -29,7 +28,6 @@ def is_winner(board, player):
 def is_terminal(board):
     return is_winner(board, HUMAN) or is_winner(board, AI) or " " not in board
 
-# Alpha-Beta Minimax
 def minimax(board, depth, alpha, beta, maximizing):
     if is_winner(board, AI):
         return 10 - depth
@@ -75,14 +73,12 @@ def get_best_move(board):
 
     return best_move
 
-# Main game loop
 def play():
     board = create_board()
     print("You are X, AI is O")
     display(board)
 
     while True:
-        # Human move
         try:
             move = int(input("Enter your move (0-8): "))
         except:
@@ -103,7 +99,6 @@ def play():
             print("Draw!")
             break
 
-        # AI move
         ai_move = get_best_move(board)
         board[ai_move] = AI
         print(f"AI plays at {ai_move}")
